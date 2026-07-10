44a6aff881294508d4bcab36ea48784db5ed97de
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-07-10 14:54:26 +0700

    Add tactile press feedback to onboarding navigation buttons.

--- opencore_flutterians/lib/onboarding/widgets/onboarding_skip_control.dart
+++ opencore_flutterians/lib/onboarding/widgets/onboarding_skip_control.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'onboarding_tactile_button.dart';
+
 class OnboardingSkipControl extends StatelessWidget {
   const OnboardingSkipControl({super.key, required this.onSkip});
 
@@ -9,7 +11,7 @@ class OnboardingSkipControl extends StatelessWidget {
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.centerRight,
-      child: TextButton(
+      child: OnboardingTextButton(
         onPressed: onSkip,
         child: Text('SKIP', style: Theme.of(context).textTheme.labelSmall),
       ),
