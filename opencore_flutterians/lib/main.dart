d8bb3d35f564308d0c2914ac9b1e3d5358422e61
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-07-20 18:33:29 +0700

    Wire home facade as post-onboarding root.

--- opencore_flutterians/lib/main.dart
+++ opencore_flutterians/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:opencore_flutterians/core/open_core_bloc_observer.dart';
+import 'package:opencore_flutterians/home/home.dart';
 import 'package:opencore_flutterians/onboarding/onboarding.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -33,46 +34,7 @@ class OpenCoreApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: OnboardingFacade().buildRoot(
-        home: const OpenCoreHomePage(title: 'OpenCore'),
-      ),
-    );
-  }
-}
-
-class OpenCoreHomePage extends StatefulWidget {
-  const OpenCoreHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<OpenCoreHomePage> createState() => _OpenCoreHomePageState();
-}
-
-class _OpenCoreHomePageState extends State<OpenCoreHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() => setState(() => _counter++);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text('$_counter', style: Theme.of(context).textTheme.headlineMedium),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        home: HomeFacade().buildRoot(),
       ),
     );
   }
